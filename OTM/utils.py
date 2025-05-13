f162b54b0b41bf2974d86cf5d2a7f5c492ec0ab1
ViníciusA <vinir570@gmail.com>
2025-05-13 12:10:32 -0300

    Arquivo com funções base dos algoritmos. Ainda não está integrado ao resto do código

---
+++ OTM/utils.py
@@ -0,0 +1,41 @@
+import random
+from classes_solucao import KPFSolution
+
+
+# Configurado para função de minimização.
+# Usar -f(x)
+def ConstructiveAlgorithm(solutions, alpha = 0.2):
+
+    h_min = float('inf')
+    h_max = -float('inf')
+    for solution in solutions:
+        objValue = solution.objective_value()
+        if (objValue < h_min):
+            h_min = objValue
+        elif (objValue > h_max):
+            h_max = objValue
+        else:
+            continue
+
+    lowerRange = h_min
+    upperRange = h_max + alpha * (h_min - h_max)
+    candidatesList = [element for element in solutions if element >= lowerRange and element <= upperRange]
+
+    return random.choice(candidatesList)
+    
+
+def BestImprovement(original_solution, neighbourhood):
+
+    best_solution = original_solution
+    best_obj_value = original_solution.objective_value()
+    convergence = False
+    while not(convergence):
+        convergence = True
+        for current_solution in neighbourhood:
+            current_obj_value = current_solution.objective_value()
+            if (current_obj_value > best_obj_value):
+                convergence = False
+                best_solution = current_solution
+    return best_solution
+        
+        
