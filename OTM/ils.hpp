96efe4f23ece3bf904d3be94c90a435c2370e06e
ViniciusA <vinir570@gmail.com>
2025-06-18 21:02:51 -0300

    Different name to work in WSL

---
+++ OTM/ils.hpp
@@ -0,0 +1,9 @@
+#ifndef ILS_HPP
+#define ILS_HPP
+
+#include "classes_base.hpp"
+
+KPFSolution PerturbSolution(const KPFSolution& solution, int strength = 1);
+KPFSolution ILS(const KPFSProblem& problem, int max_iterations = 100, int perturbation_strength = 2, float alpha = 0.8, int relaxation = 10);
+
+#endif
